f0cbf146af37931798cbfb8df3a4b02ccab4b761
Pavel Shavrov <3109955+pavliks@users.noreply.github.com>
2026-01-08 00:31:15 +0300

    Update settings.py add DEBUG

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['*']
 
